8d8b5130552912ca629c8cd877cbc7b767406394
Jesse Jackson <jesse.publicmail@gmail.com>
2015-04-30 18:21:04 -0500

    Add js file

---
+++ js/ww.js
@@ -0,0 +1,20 @@
+// Virtual Village Classroom - Weekly Writer
+
+// Variables ----------
+
+
+
+
+// Functions ----------
+
+
+
+
+// EventListeners ----------
+
+
+
+
+// Execute ----------
+
+console.log('VVC Weekly Writer');
